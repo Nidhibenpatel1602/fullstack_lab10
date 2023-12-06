d93cff97ac0cf49d000cf031675cc3d6395e394f
Nidhibenpatel1602 <146142823+Nidhibenpatel1602@users.noreply.github.com>
2023-12-05 19:21:28 -0500

    Add files via upload

--- tdd-lab-w9/src/__init__.py
+++ tdd-lab-w9/src/__init__.py
@@ -16,3 +16,24 @@ class Ping(Resource):
   }
  
 api.add_resource(Ping, '/ping')
+
+
+# src/__init__.py
+
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+# instantiate the app
+app = Flask(__name__)
+api = Api(app)
+
+# set config
+app.config.from_object('src.config.DevelopmentConfig') # new
+class Ping(Resource):
+ def get(self):
+  return {
+  'status': 'success',
+  'message': 'pong!'
+ }
+ 
+api.add_resource(Ping, '/ping')
