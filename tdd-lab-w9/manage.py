587aaf47231ad660cc929c61f469d6ce7637457c
Nidhibenpatel1602 <146142823+Nidhibenpatel1602@users.noreply.github.com>
2023-12-05 21:40:05 -0500

    Add files via upload

--- tdd-lab-w9/manage.py
+++ tdd-lab-w9/manage.py
@@ -1,13 +1,19 @@
 # manage.py
-from flask.cli import FlaskGroup
-from src import app, db # new
-cli = FlaskGroup(app)
 
-# new
+
+import sys
+from flask.cli import FlaskGroup
+from src import create_app, db # new
+from src.api.models import User # new
+
+app = create_app() # new
+cli = FlaskGroup(create_app=create_app) # new
+
 @cli.command('recreate_db')
 def recreate_db():
  db.drop_all()
  db.create_all()
  db.session.commit()
+ 
 if __name__ == '__main__':
  cli()
